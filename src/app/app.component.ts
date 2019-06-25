2fd5d15c0dcc31d5bcefea2f88278adacf26765a
Wilfred <wilcr20@gmail.com>
2019-06-25 16:39:38 -0600

    Muestra listado de pokemons atraves de lectura JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ObtieneDatosService } from '../app/servicios/obtiene-datos.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PokeBatallas';
+
+  pokemons: any;
+  movimientos: any;
+
+  constructor(private obtiene: ObtieneDatosService) {
+    this.obtiene.getPokemons().subscribe(data => {
+      this.pokemons = data;
+      console.log(this.pokemons)
+    });
+    this.obtiene.getMovimientos().subscribe(data => {
+      this.movimientos = data;
+      console.log(this.movimientos)
+    });
+  }
+
+  obtenerMovimientoPorNombre(nombre){
+    for (let index = 0; index < this.movimientos.length; index++) {
+      const element = this.movimientos[index];
+      if(element.nombre == nombre){
+        return element;
+      }
+    }
+    return null;
+  }
+
 }
+
+
+
---
+++ src/app/servicios/obtiene-datos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+//import { Observable } from 'rxjs/Observable';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ObtieneDatosService {
+
+  constructor(private http: HttpClient) {
+    // this.getPokemons().subscribe(data => {
+    //     console.log(data);
+    // });
+  }
+
+  public getPokemons(): Observable<any> {
+    return this.http.get("./assets/Datos/pokemons.json");
+  }
+
+  public getMovimientos(): Observable<any> {
+    return this.http.get("./assets/Datos/movimientos.json");
+  }
+}
